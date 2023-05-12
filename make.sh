a3231fd1c828feff192d5a7f89ec0b5655633567
cybern0id <jc@thunderbug.co.uk>
2023-05-12 21:55:08 +0100

    Build overlay tarball for rugjam and adjust make script to suit.

--- make.sh
+++ make.sh
@@ -2,4 +2,4 @@
 
 chmod 600 overlay/etc/ssh/ssh_host_*_key
 chmod +x overlay/etc/local.d/headless.start
-tar czvf headless.apkovl.tar.gz -C overlay etc --owner=0 --group=0
+tar czvf rugjam.apkovl.tar.gz -C overlay etc --owner=0 --group=0
